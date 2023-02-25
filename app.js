0c0aaafbfe255c4751a0641cedbc8db246c0236b
mcantimurcan@gmail.com <mcantimurcan@gmail.com>
2023-02-25 21:49:22 +0300

    trying to add an admin panel

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const path = require('path')
+const homeRoutes = require('./routes/homeRoutes')
+
 const app = express()
 
-app.use(express.static('assets'))
 app.set('view engine', 'ejs')
+
+app.use(express.static('assets'))
+app.use(homeRoutes.routes)
 
 app.get('/', function(req, res){
     res.render('index')
@@ -20,4 +26,4 @@ app.get('/uyeol', function(req, res){
     res.render('signup')
 })
 
-app.listen(8000)
+app.listen(8000, () => console.log('App is listening on url http://localhost:8000'))
