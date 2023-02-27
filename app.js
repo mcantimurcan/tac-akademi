aaa60c5a7f88447f53d0ef45499baeb0df43a7f0
mcantimurcan@gmail.com <mcantimurcan@gmail.com>
2023-02-27 15:24:36 +0300

    we can now add our products with an admin page

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const morgan = require('morgan')
 const expressLayouts = require('express-ejs-layouts')
-const path = require('path')
 const homeRoutes = require('./routes/homeRoutes')
+const path = require('path')
 const mongoose = require('mongoose')
 const Product = require('./models/products')
 
@@ -13,16 +14,18 @@ mongoose.connect(dbURL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then((result) => app.listen(8000))
 .catch((err) => console.log(err))
 
-
 app.set('view engine', 'ejs')
-app.use(express.static('assets'))
+
+app.use(express.static(path.join(__dirname, 'assets')))
+app.use(express.urlencoded({extended: true}))
+app.use(morgan('dev'))
 
 
 app.get('/', function(req, res){
     res.render('index', {title: 'Ana Sayfa'})
 })
 
-app.get('/about', function(req, res){
+app.get('/hakkimizda', function(req, res){
     res.render('about', {title: 'Hakkımızda'})
 })
 
@@ -43,6 +46,51 @@ app.get('/urunler', function(req, res){
         console.log(err)
     })
 })
+
+app.get('/urunler/:id', (req, res) => {
+    const id = req.params.id
+
+    Product.findById(id).then((result) => {
+        res.render('single-product', {title: 'Ürün Detayı', product: result})
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+app.get('/admin', (req, res) => {
+    Product.find().sort({createdAt: -1})
+    .then((result) => {
+        res.render('admin', {title: 'Admin Paneli', products: result})
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+})
+
+app.get('/admin/add', (req, res) => {
+    res.render('add', {title: 'Ürün Ekle'})
+})
+
+app.post('/admin/add', (req,res) => {
+    const product = new Product(req.body)
+
+    product.save().then((result) => {
+        res.redirect('/admin')
+    }).catch((err) => {
+        console.log(err)
+    })
+})
+
+app.delete('/admin/delete/:id', (req, res) => {
+    const id = req.params.id
+    Product.findByIdAndDelete(id).then((result) => {
+        res.json({link: '/admin'})
+    }).catch((err) => {
+        console.log(err)
+    })
+})
+
 
 app.use((req,res) => {
     res.status(404).render('404', {title: '404 Not Found'})
